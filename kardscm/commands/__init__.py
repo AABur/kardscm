f3eacda6bd56d07cdc5b2e3a0ae4dd146014e0b5
Alexander Burchenko <aabur@mail.ru>
2026-06-03 12:00:21 +0300

    refactor: slim down commands/__init__.py to compat re-exports

--- kardscm/commands/__init__.py
+++ kardscm/commands/__init__.py
@@ -2,16 +2,25 @@
 
 from __future__ import annotations
 
-from kardscm.commands.baseline import baseline_accept as baseline_accept
-from kardscm.commands.baseline import baseline_init as baseline_init
-from kardscm.commands.decks import add_deck as add_deck
-from kardscm.commands.decks import export_deck as export_deck
-from kardscm.commands.decks import import_deck as import_deck
-from kardscm.commands.decks import remove_deck as remove_deck
-from kardscm.commands.export import export_collection as export_collection
-from kardscm.commands.export import update_collection as update_collection
-from kardscm.commands.sync import apply_sync_changes as apply_sync_changes
-from kardscm.commands.sync import fetch_and_compute_diff as fetch_and_compute_diff
-from kardscm.commands.sync import sync_collection as sync_collection
-from kardscm.commands.utils import _emit_locale_warnings as _emit_locale_warnings
-from kardscm.commands.validation import validate_file as validate_file
+from kardscm.commands.baseline import baseline_accept, baseline_init
+from kardscm.commands.decks import add_deck, export_deck, import_deck, remove_deck
+from kardscm.commands.export import export_collection, update_collection
+from kardscm.commands.sync import apply_sync_changes, fetch_and_compute_diff, sync_collection
+from kardscm.commands.utils import _emit_locale_warnings
+from kardscm.commands.validation import validate_file
+
+__all__ = [
+    "baseline_accept",
+    "baseline_init",
+    "add_deck",
+    "export_deck",
+    "import_deck",
+    "remove_deck",
+    "export_collection",
+    "update_collection",
+    "apply_sync_changes",
+    "fetch_and_compute_diff",
+    "sync_collection",
+    "_emit_locale_warnings",
+    "validate_file",
+]
